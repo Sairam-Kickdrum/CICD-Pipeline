09b9af258ce84c55848dbb19edac5c186d51c647
Sairam Konda <sairam.konda@kickdrumtech.com>
2026-02-27 12:26:10 +0530

    Added Java Prohect files

---
+++ src/main/java/org/example/cicdjava/CicdJavaApplication.java
@@ -0,0 +1,13 @@
+package org.example.cicdjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdJavaApplication.class, args);
+    }
+
+}
